dd00ecce39ef80745a9c858bff3d311c5e09566f
bereket <gezahegnebereket@gmail.com>
2023-09-02 09:00:57 +0300

    controllers ena other funct

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,32 +6,9 @@ const checkAuth=require('../middleware/check-auth')
 
 const multer = require("multer");
 const ProductContoller = require("../controllers/products");
+const { upload } = require("../middleware/upload");
 
-const storage = multer.diskStorage({
-  function(req, file, cb) {
-    cb(null, "./uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, new Date().toDateString() + file.originalname);
-    // cb(null, new Date().toISOString() + file.originalname);
-  },
-});
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-    cb(null, true);
-  } else {
-    cb(new Error("Invalid type"), false);
-  }
-};
-
-// destination doesn't throw new error
-// const upload=multer({dest:'./uploads/'})
-const upload = multer({
-  storage: storage,
-  fileFilter: fileFilter,
-  limits: { fileSize: 1024 * 1024 * 5 },
-});
 router.get("/",ProductContoller.productGetAll);
 
 router.get("/:productId", ProductContoller.productGetOne);
